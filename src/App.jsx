7adb04eaf28bbc8b24a9ac6a6f8b542109b38893
singhhimansh <him.signum802@gmail.com>
2022-09-06 15:42:12 +0530

    add: search 🔍

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, NavLink, useSearchParams } from "react-router-dom";
 import { Button } from '@mantine/core';
 
 import logo from './images/blognobg.png';
@@ -13,18 +13,10 @@ import BlogDetails from "./BlogDetails";
 
 function App() {
 
-  let activeStyle = {
-    color: 'rgb(3 105 161)' ,
-    backgroundColor:'#e0f2fe',
-    // borderLeft: '1px solid #0284c7',
-    boxShadow: '-1px 0px 0px 0px #0284c7'
-  };
-
-
-
-
-
   const [blogList, setBlogList] = useState([]);
+
+
+  // const [searchParams,setSearchParams] = useSearchParams();
 
 
   useEffect(() => {
@@ -38,23 +30,60 @@ function App() {
     // console.log(blogList)
   }, []);
 
-  // function handleTypeSearch(type){
-  //     console.log('type clicked', type);
+  let activeStyle = {
+    color: 'rgb(3 105 161)',
+    backgroundColor: '#e0f2fe',
+    boxShadow: '-1px 0px 0px 0px #0284c7'
+  };
 
-  //     let searchedBlogs = blogList.filter((blog)=> blog.type === type );
 
-  //     console.log(searchedBlogs);
-  //     setBlogList(searchedBlogs);
+  function handleSearch(e) {
+    e.preventDefault()
+    // console.log(e);
+    // console.log(e.target[0].value);
 
-  // }
+    const search = e.target[0].value.trim().toLowerCase();
+
+    console.log('searching ', search);
+
+    const searchedBlogs = blogList.filter(({ title, author, type }) => title.toLowerCase().includes(search) || author.toLowerCase().includes(search) || type.toLowerCase().includes(search))
+
+    console.log(searchedBlogs);
+    setBlogList(searchedBlogs);
+
+
+
+
+    // let searchedBlogs = blogList.filter((blog)=> blog.type === type );
+
+    // console.log(searchedBlogs);
+    // setBlogList(searchedBlogs);
+
+  }
+
+
+
+
+
 
   return (
     <Router>
       <header className="py-5   bg-sky-200 flex gap-2 justify-around">
 
-        <div className="logo w-24"><img className='object-contain' src={logo} alt="blog logo" /></div>
+        <a href='/' className="logo w-24"><img className='object-contain' src={logo} alt="blog logo" /></a>
 
         <div className="navMenu flex items-center gap-3">
+
+          <form className="flex items-center relative" onSubmit={(e)=>handleSearch(e)}>
+            <label htmlFor="simple-search" className="sr-only">Search</label>
+
+            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-400/80 focus:border-sky-400/80 block w-full pr-8 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-400/80 dark:focus:border-sky-400/80" placeholder="Search by title, author" required />
+            
+            <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-sky-700 rounded-r-lg border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
+              <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              <span className="sr-only">Search</span>
+            </button>
+          </form>
           <Link rel="stylesheet" to="/" > <Button >Blogs</Button> </Link>
           <Link rel="stylesheet" to="/Addblog" > <Button>Add New Blog</Button> </Link>
         </div>
