31c944c9b0642c9e899dbf449ee72d64d4d2dc25
singhhimansh <him.signum802@gmail.com>
2022-09-06 21:25:02 +0530

    add: date📅

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Badge,Divider } from "@mantine/core";
+import { Badge, Divider } from "@mantine/core";
 
 
 
@@ -13,7 +13,7 @@ export default function BlogDetails({ blogList }) {
 
         const clickedBlog = blogList.filter((blog) => blog.id == id);
         setBlog(clickedBlog[0])
-    },[]);
+    }, []);
 
 
     return (
@@ -22,11 +22,13 @@ export default function BlogDetails({ blogList }) {
                 <span className=""><Link className="type font-mono text-xs" to={`/filteredblogs/${blog.type}`} ><span className="inline-block"><Badge variant="outline">
                     {blog.type}
                 </Badge></span></Link></span>
-                <h1 className="title text-3xl text-slate-700 font-bold first-letter:capitalize">{blog.title}</h1>
-                <h4 className="w-1/2 flex text-sm text-slate-600 capitalize gap-5 justify-between">
-                    <p className="author capitalize">{blog.author}</p>
-                    <p className="date">{blog.date}</p>
-                </h4>
+                <h1 className="title my-2 text-3xl text-slate-700 font-bold first-letter:capitalize">{blog.title}</h1>
+                <div className="w-1/2 flex text-xs text-slate-600 gap-2">
+                    <span> By</span>
+                    <p className="author capitalize ">{blog.author}</p>
+                    on <IconCalendarTime className="inline h-4 self-center" />
+                    <p className="date ">{blog.date}</p>
+                </div>
                 <Divider my="sm" variant="dashed" />
                 <p className="blogContent mt-8 text-slate-600" dangerouslySetInnerHTML={{ __html: `${blog.blogContent}` }} />
             </div>
